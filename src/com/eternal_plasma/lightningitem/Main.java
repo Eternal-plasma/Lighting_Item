e9fab65e0fdc80d1f485519ce1431f9fded3b2a9
Eternal_plasma <eternal.plasma1634@gmail.com>
2020-06-27 00:21:35 -0500

    change

--- src/com/eternal_plasma/lightningitem/Main.java
+++ src/com/eternal_plasma/lightningitem/Main.java
@@ -1,5 +1,33 @@
 package com.eternal_plasma.lightningitem;
 
-public class Main {
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.permissions.PermissionAttachment;
+import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.HashMap;
+import java.util.UUID;
+
+public class Main extends JavaPlugin implements Listener {
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        Player player = event.getPlayer();
+
+
+        if (player.getInventory().getItemInMainHand().getType() == Material.STICK) {
+            player.getWorld().strikeLightning(player.getTargetBlock(null, 200).getLocation());
+
+        }
+
+    }
 }
